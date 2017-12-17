fb6efb7fd8076b6ab7a900a6ba698ff704046c99
Farhad <Mehryari.f@gmail.com>
2017-12-18 00:55:53 +0330

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Hi people</h1>
+    <p>Iranian JavaScript Developers Community</p>
+    <p>web site will be completed soon.</p>
+    <Link to="/members/">Members</Link>
+  </div>
+)
+
+export default IndexPage
---
+++ src/pages/members.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const SecondPage = () => (
+  <div>
+    <h1>Members</h1>
+    <p>here will be the list of Iranian JavaScript Developers</p>
+    <Link to="/">Go back to the homepage</Link>
+  </div>
+)
+
+export default SecondPage
